6b89c3bed5daa73cdf1d514792b641a97ac7cf6d
Your Name <you@example.com>
2023-01-06 09:43:14 +0530

    new file

---
+++ Square of numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(j=1;j<=n;j++)
+    {
+        for(i=j;i<=(2*i-1);i++)
+        {
+            printf("%d\n",i);
+
+        }
+    }
+    return 0;
+}
